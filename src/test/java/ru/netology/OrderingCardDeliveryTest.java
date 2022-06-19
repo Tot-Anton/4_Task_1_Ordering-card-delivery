3e3e80eba771ded71ec08543ce3ef9aa9ad92285
Anton Belmisov <antonbelmisov@mail.ru>
2022-06-19 16:35:03 +0300

    valid last name and first name do not pass

--- src/test/java/ru/netology/OrderingCardDeliveryTest.java
+++ src/test/java/ru/netology/OrderingCardDeliveryTest.java
@@ -90,12 +90,115 @@ public class OrderingCardDeliveryTest {
         $("[data-test-id=city] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
+    @Test
+    //Тестирование функциональности (указываем не полный телефон)
+    public void testedFunctionalityV4() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Пушкин Александр");
+        $("[data-test-id=phone] input").setValue("+792000011");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
+    @Test
+    //Тестирование функциональности (телефон с 8ки)
+    public void testedFunctionalityV5() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Пушкин Александр");
+        $("[data-test-id=phone] input").setValue("89200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
+    @Test
+    //Тестирование функциональности (телефон с + и 4ки)
+    public void testedFunctionalityV6() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Пушкин Александр");
+        $("[data-test-id=phone] input").setValue("+49200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        //shouldBe - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        //Duration.ofSeconds - получает длительность, представляющую количество секунд
+        $("[data-test-id=notification]").shouldBe(appear, Duration.ofSeconds(15));
+        //shouldHave - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        $(".notification__title").shouldHave(exactText("Успешно!"));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateOfTheMeeting));
+    }
+
+    @Test
+    //Тестирование функциональности (указываем двойное фамилия)
+    public void testedFunctionalityV7() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Пушкин-Толстой Александр");
+        $("[data-test-id=phone] input").setValue("+79200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        //shouldBe - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        //Duration.ofSeconds - получает длительность, представляющую количество секунд
+        $("[data-test-id=notification]").shouldBe(appear, Duration.ofSeconds(15));
+        //shouldHave - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        $(".notification__title").shouldHave(exactText("Успешно!"));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateOfTheMeeting));
+    }
+
+    @Test
+    //Тестирование функциональности (в поле ФИ указаны цифры)
+    public void testedFunctionalityV8() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Пушкин4 Александр5");
+        $("[data-test-id=phone] input").setValue("+79200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    //Тестирование функциональности (в поле ФИ латиницей)
+    public void testedFunctionalityV9() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Vstanya Zina");
+        $("[data-test-id=phone] input").setValue("+79200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    //Тестирование функциональности (в поле ФИ присутствует "Ё")
+    public void testedFunctionalityV10() {
+        $("[data-test-id=city] input").setValue("Владимир");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(dateOfTheMeeting);
+        $("[data-test-id=name] input").setValue("Ёлкин Пчёлкин");
+        $("[data-test-id=phone] input").setValue("+79200001100");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        //shouldBe - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        //Duration.ofSeconds - получает длительность, представляющую количество секунд
+        $("[data-test-id=notification]").shouldBe(appear, Duration.ofSeconds(15));
+        //shouldHave - подождите, пока данный элемент не удовлетворит заданному условию (с заданным таймаутом)
+        $(".notification__title").shouldHave(exactText("Успешно!"));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateOfTheMeeting));
+    }
 
     @Test
     //Тестирование функциональности (чек-бокс не отмечен)
-    public void testedFunctionalityV10() {
+    public void testedFunctionalityV() {
 
     }
 
